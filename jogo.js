a418b64fa135c76fd8a5374e4b1076067f9bab96
Maino <anacarolinesarre@gmail.com>
2019-08-15 13:51:53 -0300

    Game mata mosquito, definida dimençao do jogo e criaçao de posiçoes randomicas

---
+++ jogo.js
@@ -0,0 +1,30 @@
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo(){
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(altura, largura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica(){
+
+    var posicaox = Math.floor(Math.random() * largura) -90
+    var posicaoy = Math.floor(Math.random() * altura)-90
+
+    posicaox = posicaox < 0 ? 0 : posicaox
+    posicaoy = posicaoy < 0 ? 0 : posicaoy
+
+    console.log( posicaox, posicaoy )
+
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = 'mosquito-1'
+    mosquito.style.left = posicaox+ 'px'
+    mosquito.style.top = posicaoy + 'px'
+    mosquito.style.position = 'absolute'
+document.body.appendChild(mosquito)
+}
