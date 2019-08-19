2a804a6d6b0e45da817fecea48f9b2055bc71384
Maino <anacarolinesarre@gmail.com>
2019-08-19 16:25:23 -0300

    niveis de dificuldade

--- jogo.js
+++ jogo.js
@@ -3,6 +3,19 @@ var largura = 0
 var vidas = 1
 var tempo = 10
 
+var criamosquitotempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal'){
+    criamosquitotempo = 1500
+} else if(nivel === 'dificil'){
+    criamosquitotempo = 1000
+} else if (nivel === 'chuck'){
+    criamosquitotempo = 750
+}
+
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
     largura = window.innerWidth
